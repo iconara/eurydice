33a82d4f84bb20222af74e5b644556571c5bb6f4
Theo <theo@iconara.net>
2011-08-06 13:43:57 +0200

    Updated one of the examples

--- examples/insert.rb
+++ examples/insert.rb
@@ -1,16 +1,22 @@
+# encoding: utf-8
+
 $: << File.expand_path('../../lib', __FILE__)
 
 require 'eurydice'
 
 
 begin
-  keyspace = Eurydice.connect('blurgh')
+  cluster = Eurydice.connect
+  keyspace = cluster.keyspace('blurgh', :create => false)
+  keyspace.drop! rescue nil
   keyspace.create!
-
-  cf = keyspace.column_family('foo')
-  cf.create!
-  
-  cf.update('HELLOWORLD', {'foo' => 'bar'})
+  cf = keyspace.column_family('foo', :create => false)
+  cf.create!(:key_validation_class => :ascii, :comparator_type => :ascii, :default_validation_class => :utf8)
+  cf.update('HELLOWORLD', {'foo' => 'bar', 'hello' => 'wörld'})
+  p cf.get('HELLOWORLD', :columns => %w(hello))
+rescue Eurydice::EurydiceError => e
+  $stderr.puts("#{e.message} (#{e.class})")
+  $stderr.puts("\t" + e.backtrace.join("\n\t"))
 ensure
   keyspace.drop!
 end
