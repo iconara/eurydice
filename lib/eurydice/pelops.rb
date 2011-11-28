874324569e7e16721f0a016e0559d7c10eacb78f
Theo <theo@iconara.net>
2011-11-28 17:06:29 +0100

    Added support for enabling dynamic node discovery

--- lib/eurydice/pelops.rb
+++ lib/eurydice/pelops.rb
@@ -29,7 +29,8 @@ module Eurydice
       host = options.fetch(:host, 'localhost')
       port = options.fetch(:port, 9160)
       pool_name = options.fetch(:pool_name, 'eurydice')
-      Cluster.new(::Pelops::Cluster.new(host, port))
+      dynamic_node_discovery = options.fetch(:dynamic_node_discovery, false)
+      Cluster.new(::Pelops::Cluster.new(host, port, dynamic_node_discovery))
     end
   
     def self.keyspace(keyspace_name, host='localhost', port=9160, pool_name='eurydice')
