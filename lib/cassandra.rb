5309e941085fa720628cab588237e59747aac5f5
Theo <theo@iconara.net>
2011-11-29 13:03:16 +0100

    Added more consistency levels

--- lib/cassandra.rb
+++ lib/cassandra.rb
@@ -17,10 +17,14 @@ module Cassandra
   import 'org.apache.cassandra.thrift.IndexOperator'
   
   CONSISTENCY_LEVELS = {
-    :one    => Cassandra::ConsistencyLevel::ONE,
-    :quorum => Cassandra::ConsistencyLevel::QUORUM,
-    :all    => Cassandra::ConsistencyLevel::ALL,
-    :any    => Cassandra::ConsistencyLevel::ANY
+    :any          => Cassandra::ConsistencyLevel::ANY,
+    :one          => Cassandra::ConsistencyLevel::ONE,
+    :two          => Cassandra::ConsistencyLevel::TWO,
+    :three        => Cassandra::ConsistencyLevel::THREE,
+    :local_quorum => Cassandra::ConsistencyLevel::LOCAL_QUORUM,
+    :each_quorum  => Cassandra::ConsistencyLevel::EACH_QUORUM,
+    :quorum       => Cassandra::ConsistencyLevel::QUORUM,
+    :all          => Cassandra::ConsistencyLevel::ALL
   }.freeze
 
   MARSHAL_TYPES = {
