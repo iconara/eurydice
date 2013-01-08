c531806a6342d412380abb57e2ca25fe7e91c0b4
Theo <theo@iconara.net>
2013-01-08 11:40:21 +0100

    Forgot to att CASSANDRA_HOST to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ $: << File.expand_path('../../lib', __FILE__)
 require 'bundler/setup'
 require 'eurydice/pelops'
 
+ENV['CASSANDRA_HOST'] ||= 'localhost'
+
 require_relative 'eurydice/support/cluster'
 require_relative 'eurydice/support/column_family'
 require_relative 'eurydice/support/keyspace'
